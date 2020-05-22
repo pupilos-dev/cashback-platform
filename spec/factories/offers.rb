20a6080012c0a4a6aa3530a24593f4446f521437
Renata Marques <renata.nrmarques@gmail.com>
2020-05-21 23:11:00 -0300

    adding responsiveness

--- spec/factories/offers.rb
+++ spec/factories/offers.rb
@@ -9,4 +9,38 @@ FactoryBot.define do
     premium { false }
     association :member
   end
+
+  factory :offer_premium, class: 'Offer' do
+    advertiser_name { FFaker::Company.name }
+    url { FFaker::Internet.http_url }
+    description { FFaker::Lorem.characters(255) }
+    starts_at { Date.current }
+    ends_at { Date.tomorrow }
+    premium { true }
+  end
+
+  factory :offer_with_url_invalid, class: 'Offer' do
+    advertiser_name { FFaker::Company.name }
+    url { FFaker::Company.name }
+    description { FFaker::Lorem.characters(255) }
+    starts_at { Date.current }
+    ends_at { Date.tomorrow }
+    premium { true }
+  end
+
+  factory :offer_without_advertiser_name, class: 'Offer' do
+    url { FFaker::Internet.http_url }
+    description { FFaker::Lorem.characters(255) }
+    starts_at { Date.current }
+    ends_at { Date.tomorrow }
+    premium { true }
+  end
+
+  factory :offer_expired, class: 'Offer' do
+    advertiser_name { FFaker::Company.name }
+    url { FFaker::Internet.http_url }
+    description { FFaker::Lorem.characters(255) }
+    starts_at { Date.current.advance(months: -1) }
+    ends_at { Date.yesterday }
+  end
 end
